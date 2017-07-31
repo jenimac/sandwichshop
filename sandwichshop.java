d0754b6bec198d303e8039e6088643cd0f9a0394
jenimac <jenimac@ymail.com>
2017-07-31 14:33:35 -0400

    Add files via upload

--- sandwichshop.java
+++ sandwichshop.java
@@ -6,60 +6,60 @@ public class sandwichshop
     public static void main(String[] arg)
     {
         //Don't change these lines.
-    Scanner keyboard = new Scanner(System.in);
-    int goalForVeggies = 50;
-    int goalForBurgers = 250;
-    int goalForSubs = 180;
-    int goalForSoup = 70;
+        Scanner keyboard = new Scanner(System.in);
+        int goalForVeggies = 50;
+        int goalForBurgers = 250;
+        int goalForSubs = 180;
+        int goalForSoup = 70;
     
-    out.println("Checking sales goals.");
-    out.println("The sales goal for veggie sandwiches is 50");
-    out.println("How many veggie sandwiches were sold today?");
-    int veggie;
-    veggie = keyboard.nextInt();
-    out.println("");
+            out.println("Checking sales goals.");
+            out.println("The sales goal for veggie sandwiches is " + goalForVeggies);
+            out.println("How many veggie sandwiches were sold today?");
+            int veggie;
+            veggie = keyboard.nextInt();
+            out.println("");
     if (veggie < goalForVeggies)
     {
         out.println("Fell short for veggies.");
     }else{
         out.println("Made goal for veggies.");
     }    
-    out.println("The sales goal for burgers is 250");
-    out.println("How many burgers were sold today?");
-    int burgers;
-    burgers = keyboard.nextInt();
-    out.println("");
+            out.println("The sales goal for burgers is " + goalForBurgers);
+            out.println("How many burgers were sold today?");
+            int burgers;
+            burgers = keyboard.nextInt();
+            out.println("");
     if (burgers < goalForBurgers)
     {
         out.println("Fell short for burgers.");
     }else{
         out.println("Made goal for burgers.");
     }  
-    out.println("The sales goal for subs is 180");
-    out.println("How many subs were sold today?");
-    long subs;
-    subs = keyboard.nextInt();
-    out.println("");
+            out.println("The sales goal for subs is " + goalForSubs);
+            out.println("How many subs were sold today?");
+            int subs;
+            subs = keyboard.nextInt();
+            out.println("");
     if (subs < goalForSubs)
     {
         out.println("Fell short for subs.");
     }else{
         out.println("Made goal for subs.");
     }
-    out.println("The sales goal for soup is 70");
-    out.println("How many soups were sold today?");
-    int soup;
-    soup = keyboard.nextInt();
-    out.println("");
+            out.println("The sales goal for soup is " + goalForSoup);
+            out.println("How many soups were sold today?");
+            int soup;
+            soup = keyboard.nextInt();
+            out.println("");
     if(soup < goalForSoup)
     {
         out.println("Fell short for soup.");
     }else{
         out.println("Made goal for soup.");
     }    
-    if((veggie >= goalForVeggies) && (burgers >= goalForBurgers) && (subs >= goalForSubs) && (soup >= goalForSoup))
-    {
-        out.print("Made goal for everything!");
-    }
+                if((veggie >= goalForVeggies) && (burgers >= goalForBurgers) && (subs >= goalForSubs) && (soup >= goalForSoup))
+                {
+                    out.print("Made goal for everything!");
+                }
 }
 }
